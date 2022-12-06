354411d298733745fd5aeccfee0e60a8791ac0ae
Niklas Larsson <niklas@mm.st>
2022-12-06 10:05:55 +0100

    Day6

---
+++ haskell/app/Day6.hs
@@ -0,0 +1,14 @@
+import Data.List
+
+main = do
+    input <- inputData
+    print (findPacket 4 input)
+    print (findPacket 14 input)
+
+inputData = readFile "D:/Niklas/repos/aoc2022/data/input6.txt"
+
+allUnique xs = length (nub xs) == length xs
+
+windows n xs = [ take n m | m <- tails xs ]
+
+findPacket n xs = fmap (n+) $ findIndex allUnique (windows n xs)
