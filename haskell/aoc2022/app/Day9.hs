4b3fdcec53b2c45667f394fbe9561a9d233e3bb2
Niklas Larsson <niklas@mm.st>
2022-12-09 15:43:10 +0100

    Clean up day 9

--- haskell/aoc2022/app/Day9.hs
+++ haskell/aoc2022/app/Day9.hs
@@ -1,9 +1,8 @@
 
+import Data.Bifunctor(second)
 import qualified Data.Set as S
 
 inputData = readFile "D:/Niklas/repos/aoc2022/data/input9.txt"
-
-type Pos = (Int, Int)
 
 main = do
     input <- inputData
@@ -14,16 +13,14 @@ main = do
     print $ length visited
 
 parseLine :: String -> (String, Int)
-parseLine str = (a, read b)
-    where
-        (a, b) = break (== ' ') str
+parseLine str = second read $ break (== ' ') str
 
 move "R" = \(x,y)->(x+1,y)
 move "L" = \(x,y)->(x-1,y)
 move "U" = \(x,y)->(x,y+1)
 move "D" = \(x,y)->(x,y-1)
 
-posDiff (x1,y1) (x2, y2) = (x1-x2, y1-y2)
+delta (x1,y1) (x2, y2) = (x1-x2, y1-y2)
 
 tailMove (x, y) | abs x < 2 && abs y < 2 = id
                 | abs x > 2 || abs y > 2 = error "detached tail"
@@ -35,4 +32,5 @@ evolve state (dir, count) = go count state
         go n (rope, visited) = go (n-1) (reverse revRope, S.insert (head revRope) visited)
             where
                 newHead = move dir (head rope)
-                revRope = foldl (\nr t -> (tailMove (posDiff (head nr) t) t):nr) [newHead] (tail rope)
+                revRope = foldl (\nr t -> (update nr t):nr) [newHead] (tail rope)
+                update rope knot = tailMove (delta (head rope) knot) knot
