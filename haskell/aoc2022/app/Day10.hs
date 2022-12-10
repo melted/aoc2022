27a08d7baa7a7bd0b902efbe84aab88782491760
Niklas Larsson <niklas@mm.st>
2022-12-10 14:51:44 +0100

    clean up indexing a bit

--- haskell/aoc2022/app/Day10.hs
+++ haskell/aoc2022/app/Day10.hs
@@ -9,7 +9,7 @@ main = do
     input <- inputData
     let prog = parse input
     let output = M.fromList $ execProg prog
-    let result = sum $ map (\c -> score c $ fromJust $ M.lookupLE c output) [20, 60 .. 220]
+    let result = sum $ map (\c -> score c $ fromJust $ M.lookupLT c output) [20, 60 .. 220]
     print result
     forM (display output) $ putStrLn
 
@@ -25,7 +25,7 @@ parseInstruction err = error $ "invalid instruction: " ++ err
 exec (cc, regX) Noop = (cc+1, regX)
 exec (cc, regX) (Addx n) = (cc+2, regX+n)
 
-execProg = scanl exec (1,1)
+execProg = scanl exec (0,1)
 
 score n (_, reg) = n*reg
 
@@ -34,7 +34,7 @@ chunk n xs = splitter [] xs
         splitter acc [] = reverse acc
         splitter acc xs = let (a, b) = splitAt n xs in splitter (a:acc) b
 
-display m = chunk 40 $ map displayCycle [1..240]
+display m = chunk 40 $ map displayCycle [0..239]
     where
         reg n = fromJust $ snd <$> M.lookupLE n m
-        displayCycle n = if abs (reg n - mod (n-1) 40) <= 1 then '#' else '.'
+        displayCycle n = if abs (reg n - mod n 40) <= 1 then '#' else '.'
