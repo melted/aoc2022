5668e8f68903f273ed36aedc0e968fae92c3c22b
Niklas Larsson <niklas@mm.st>
2022-12-10 13:41:29 +0100

    Day 10

---
+++ haskell/aoc2022/app/Day10.hs
@@ -0,0 +1,40 @@
+import Data.List
+import Data.Maybe
+import Data.Traversable
+import qualified Data.Map as M
+
+inputData = readFile "D:/Niklas/repos/aoc2022/data/input10.txt"
+
+main = do
+    input <- inputData
+    let prog = parse input
+    let output = M.fromList $ execProg prog
+    let result = sum $ map (\c -> score c $ fromJust $ M.lookupLE c output) [20, 60 .. 220]
+    print result
+    forM (display output) $ putStrLn
+
+data Instruction = Noop | Addx Int deriving(Eq, Ord, Show)
+
+parse :: String -> [Instruction]
+parse input = map parseInstruction (lines input) 
+
+parseInstruction "noop" = Noop
+parseInstruction x | ["addx", arg] <- words x = Addx $ read arg
+parseInstruction err = error $ "invalid instruction: " ++ err
+
+exec (cc, regX) Noop = (cc+1, regX)
+exec (cc, regX) (Addx n) = (cc+2, regX+n)
+
+execProg = scanl exec (1,1)
+
+score n (_, reg) = n*reg
+
+chunk n xs = splitter [] xs
+    where 
+        splitter acc [] = reverse acc
+        splitter acc xs = let (a, b) = splitAt n xs in splitter (a:acc) b
+
+display m = chunk 40 $ map displayCycle [1..240]
+    where
+        reg n = fromMaybe 1 $ snd <$> M.lookupLE n m
+        displayCycle n = if abs (reg n - mod (n-1) 40) <= 1 then '#' else '.'
